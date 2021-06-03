d7cd932fa78710f71f3b4894dfae76d7dd4f12e4
Chun-Yang258 <cyang258@uwo.ca>
2021-06-03 03:55:24 -0400

    Finished About page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const big_title = document.querySelector(".big-title");
 const view_work = document.querySelector(".view-work");
 const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
+const shadow2 = document.querySelector(".shadow2");
 const content = document.querySelector(".content");
 const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
@@ -24,17 +25,17 @@ window.addEventListener('scroll', () => {
     });
 
     opacity.forEach(element => {
-        element.style.opacity = (scroll / (sectionY.top + section_height) + 0.2) > 1 ? 1 : scroll / (sectionY.top + section_height) + 0.2;
+        element.style.opacity = (sectionY.top + section_height) < 0 ? 1 : (scroll / (sectionY.top + section_height) + 0.2) > 1 ? 1 : scroll / (sectionY.top + section_height) + 0.2;
     })
 
     big_title.style.opacity = - scroll / (header_height / 2) + 1;
     view_work.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
-
+    shadow2.style.height = `${1350 - scroll * 0.6}px`;
     //content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
     //image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
     border.style.width = (scroll  / (sectionY.top + section_height) * 30) >= 15 ? "15%" : `${scroll  / (sectionY.top + section_height) * 30}%`;
     border2.style.width = (scroll  / (sectionY.top + section_height) * 30 - 20) >= 15 ? "15%" : `${scroll  / (sectionY.top + section_height) * 30 - 20}%`;
-    border3.style.width = (scroll  / (sectionY.top + section_height) * 30 -40) >= 15 ? "15%" : `${scroll  / (sectionY.top + section_height) * 30 - 40}%`;
+    border3.style.width = (scroll  / (sectionY.top + section_height) * 30 - 40) >= 15 ? "15%" : `${scroll  / (sectionY.top + section_height) * 30 - 40}%`;
 })
